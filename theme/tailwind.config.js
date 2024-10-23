0b40db004259662d772776286b2968e49b71874e
izzudd <36723840+izzudd@users.noreply.github.com>
2024-10-23 10:32:31 +0700

    implement index page

--- theme/tailwind.config.js
+++ theme/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./*.hbs', './partials/*.hbs'],
+  content: ['./*.hbs', './partials/**/*.hbs'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': 'var(--text)',
+        'background': 'var(--background)',
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+        'accent': 'var(--accent)',
+        'border': 'var(--border)',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
